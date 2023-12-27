23d348632ae435e936beec5018c84d24160b0a54
DaniIliev <114102749+DaniIliev@users.noreply.github.com>
2023-12-27 11:02:41 +0200

    add CardWapper and use Suspense

--- .next/server/webpack-runtime.js
+++ .next/server/webpack-runtime.js
@@ -125,7 +125,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	(() => {
-/******/ 		__webpack_require__.h = () => ("27838dc225250636")
+/******/ 		__webpack_require__.h = () => ("462510185b4a82a6")
 /******/ 	})();
 /******/ 	
 /******/ 	/* webpack/runtime/hasOwnProperty shorthand */
--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "296b5823b66f5294"; }
+/******/ 		__webpack_require__.h = function() { return "ebf911d98cf8c1d0"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
